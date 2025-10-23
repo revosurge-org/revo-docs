665732e7ef62f2640da7b0ccea59e6552e0c725f
logic <logic@Logicinhk.local>
2025-10-23 16:03:43 +0800

    Add GitHub Action to deploy Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 
-import vue from '@astrojs/vue';
-import { defineConfig } from 'astro/config';
+import vue from "@astrojs/vue";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	// Enable Vue to support Vue components.
-	integrations: [vue()],
+  site: "https://revo-docs.github.io",
+  base: "/",
+  // Enable Vue to support Vue components.
+  integrations: [vue()],
 });
